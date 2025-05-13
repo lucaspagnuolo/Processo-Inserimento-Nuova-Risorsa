04dcd85170e4d3f3d53c4c2287c4ecf9a5a19cad
lucaspagnuolo <luca.spagnuolo.ext@consip.it>
2025-05-13 14:55:17 +0200

    Update Processo_Nuova_Risorsa.py

--- Processo_Nuova_Risorsa.py
+++ Processo_Nuova_Risorsa.py
@@ -134,7 +134,7 @@ data_operativa = st.text_input("Giorno in cui diventa operativo (gg/mm/aaaa):", 
 # ------------------------------------------------------------
 # Preview Messaggio
 # ------------------------------------------------------------
-if st.button("Anteprima Messaggio"):
+if st.button("Template per Posta Elettronica"):
     sAM = genera_samaccountname(nome, cognome, secondo_nome, secondo_cognome, False)
     cn  = build_full_name(cognome, secondo_cognome, nome, secondo_nome, False)
     table_md = f"""
