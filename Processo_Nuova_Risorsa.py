f48e7cd3e9a969e4336bd45ffcdc1296e1ee10cd
lucaspagnuolo <luca.spagnuolo.ext@consip.it>
2025-06-27 15:23:11 +0200

    Update Processo_Nuova_Risorsa.py

--- Processo_Nuova_Risorsa.py
+++ Processo_Nuova_Risorsa.py
@@ -50,6 +50,7 @@ o365_groups = [
     defaults.get("grp_o365_teams", "O365 Teams Premium"),
     defaults.get("grp_o365_copilot", "O365 Copilot Plus"),
     defaults.get("grp_o365_viva", "O365 VivaEngage")
+    defaults.get("grp_o365_forms", "O365 Forms")
 ]
 grp_foorban = defaults.get("grp_foorban", "Foorban_Users")
 pillole = defaults.get("pillole", "Pillole formative Teams Premium")
